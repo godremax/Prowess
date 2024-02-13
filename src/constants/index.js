76ff71da1bf5addef9acdc4591a46e1bde0fb9e8
godremax <67501681+godremax@users.noreply.github.com>
2024-02-14 01:26:59 +0530

    Add files via upload

---
+++ src/constants/index.js
@@ -0,0 +1,190 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "product",
+    title: "Product",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Efficiency",
+    content:
+      " Swift Development & Cost Efficiency for a lower TCO Tangible, Business-Boosting Results",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "Delivery",
+    content:
+      "Agile innovation & Turbocharged Digital Revolution, Packaged Offerings with a Customer-Centric Approach",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Modernization",
+    content:
+      "Effortless Showcase of MuleSoft ROI, Streamlined maintenance and Legacy modernization",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "I am delighted to share the partnership with Prowess Software Services has matured in many ways and is making waves in delivering customer implementations with diligence and high quality.",
+    name: "Pratap Pillai",
+    title: "Integration COE Delivery Manager CEVA logistic",
+    img: people01,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "Over the past three years, Prowess team has partnered with Keystone to deliver some of the most notable successes in Omnichannel fulfillment for leading retailers in North America.",
+    name: "Ravi Joshi",
+    title: "Managing Partner Keystone Logistics",
+    img: people02,
+  },
+  
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "Certified Consultation",
+    value: "300+",
+  },
+  {
+    id: "stats-2",
+    title: "End-to-end solutions",
+    value: "360°",
+  },
+  {
+    id: "stats-3",
+    title: "COE for integration",
+    value: "100%",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+        link: "https://www.hoobank.com/content/",
+      },
+      {
+        name: "How it Works",
+        link: "https://www.hoobank.com/how-it-works/",
+      },
+      {
+        name: "Create",
+        link: "https://www.hoobank.com/create/",
+      },
+      {
+        name: "Explore",
+        link: "https://www.hoobank.com/explore/",
+      },
+      {
+        name: "Terms & Services",
+        link: "https://www.hoobank.com/terms-and-services/",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+        link: "https://www.hoobank.com/help-center/",
+      },
+      {
+        name: "Partners",
+        link: "https://www.hoobank.com/partners/",
+      },
+      {
+        name: "Suggestions",
+        link: "https://www.hoobank.com/suggestions/",
+      },
+      {
+        name: "Blog",
+        link: "https://www.hoobank.com/blog/",
+      },
+      {
+        name: "Newsletters",
+        link: "https://www.hoobank.com/newsletters/",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partner",
+        link: "https://www.hoobank.com/our-partner/",
+      },
+      {
+        name: "Become a Partner",
+        link: "https://www.hoobank.com/become-a-partner/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
+  },
+];
